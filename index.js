695a14c4dba118225c5947ec5e24be6a8e278729
elegant-delimiter-3950 <kinnari1022@yahoo.com>
2020-07-05 14:56:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push("Ralph");
+  return kittens;
+  
+}
+
+function destructivelyRemoveLastKitten(name)
+{
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) 
+{
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name)
+{
+  var kittens2 = kittens.concat("Broom");
+  return kittens2;
+}
+
+function prependKitten(name, array)
+{
+ return ["Arnold"...kittens];
+  
+}
+
+
+
+
 
 // Add your functions and code here
